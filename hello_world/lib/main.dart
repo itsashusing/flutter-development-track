3d0b59b32768e782f5dc2d706f63dee3790298b5
Ashutosh Singh <itsmemyself710@gmail.com>
2024-09-08 12:45:39 +0530

    stateless widget demo

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My new Hello world app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: const Center(
+          child: Text(
+            "Welcome to flutter developent",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
